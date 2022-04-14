bb28aa7dbee50463c6d8dedcb284f536215de4bb
KDA <ka.dmitriy@gmail.com>
2022-04-14 23:47:19 +0300

    add application

---
+++ src/main/java/app/kadmitriy/springsimpleapp/SpringSimpleappApplication.java
@@ -0,0 +1,13 @@
+package app.kadmitriy.springsimpleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSimpleappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSimpleappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/kadmitriy/springsimpleapp/SpringSimpleappApplicationTests.java
@@ -0,0 +1,13 @@
+package app.kadmitriy.springsimpleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSimpleappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
